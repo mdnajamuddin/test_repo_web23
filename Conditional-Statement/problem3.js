0b8b26d04f6ed692b658dee80791d33eaf3cdb32
mdnajamuddin <mdnajamuddinnautan@gmail.com>
2022-10-11 00:02:38 +0530

    commit message

---
+++ Conditional-Statement/problem3.js
@@ -0,0 +1,5 @@
+let num1 = 5;
+let num2 = 5;
+if (num1 == num2){
+  console.log("both equal")
+}
---
+++ Conditional-Statement/problem4.js
@@ -0,0 +1,17 @@
+let username_data = "rehan@gmail.com";
+let userpass_data = "Ab12";
+
+let username = "rehan@gmail.com";
+let pass = "Ab12";
+
+if (username_data == username){
+  if (userpass_data == pass){
+    console.log("User Login")
+    
+  }else {
+    console.log("Password Not Mach")
+  }
+  
+}else {
+  console.log("User Name not mach")
+}
